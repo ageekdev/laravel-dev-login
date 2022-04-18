5873c9b0365e872c5da63b335eeac00a2aed92da
Tint Naing Win <amigo.k8@gmail.com>
2022-04-19 02:10:56 +0700

    chore(test): fix default user

--- tests/Datasets/Login.php
+++ tests/Datasets/Login.php
@@ -1,20 +1,18 @@
 <?php
 
 dataset('logins', [
-
     [
         'email' => "genie-dev@geniefintech.com",
         'password' => "6R0XMNEU",
-
     ],
 ]);
 dataset('invalid_logins', [
-
     [
         'email' => "genie-dev@geniefintech.com",
         'password' => "wrong_password",
 
-    ], [
+    ],
+    [
         'email' => "wrong_email",
         'password' => "wrong_password",
 
