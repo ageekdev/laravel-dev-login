853b01e5fd27c6779531f129f4422e2cbd366757
tintnaingwinn <tintnaingwinn@users.noreply.github.com>
2022-04-18 18:48:32 +0000

    Fix styling

--- src/Commands/CreateDevUserCommand.php
+++ src/Commands/CreateDevUserCommand.php
@@ -96,10 +96,10 @@ class CreateDevUserCommand extends Command
 
     private function varExport(array $context): string
     {
-        $export = var_export($context, TRUE);
+        $export = var_export($context, true);
         $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
         $array = preg_split("/\r\n|\n|\r/", $export);
-        $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+        $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [null, ']$1', ' => ['], $array);
 
         return join(PHP_EOL, array_filter(["["] + $array));
     }
