2869ec70e168a911071c71f8d96012981d8dbba0
tintnaingwin-genie <tintnaingwin-genie@users.noreply.github.com>
2022-04-22 08:04:41 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Arr;
 
-if (!function_exists('dev_user_emails')) {
+if (! function_exists('dev_user_emails')) {
     function dev_user_emails(): array
     {
-        return Arr::pluck(config('dev-login.users'),'email');
+        return Arr::pluck(config('dev-login.users'), 'email');
     }
 }
