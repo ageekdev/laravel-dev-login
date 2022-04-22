f856e8a620c05ecffb8d445566a935d4e2223be9
Tint Naing Win <amigo.k8@gmail.com>
2022-04-22 15:04:19 +0700

    chore: add UseDevLoginGuard

---
+++ src/Http/Middleware/UseDevLoginGuard.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GenieFintech\DevLogin\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UseDevLoginGuard
+{
+    /**
+     * @param Request $request
+     * @param  Closure  $next
+     * @param string|null $guard
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, string $guard = null): mixed
+    {
+        auth()->setDefaultDriver(config('dev-login.auth.guard_name'));
+
+        return $next($request);
+    }
+}
