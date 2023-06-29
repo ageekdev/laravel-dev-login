9f3b910b7997ab7aeefc38bd66d2fab12e6f29fb
Tint Naing Win <amigo.k8@gmail.com>
2023-06-29 12:19:48 +0700

    fix:code style

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -18,18 +18,18 @@ class DashboardController extends Controller
             [
                 'route_name' => 'telescope',
                 'title' => 'Telescope',
-                'desc' => "Debug your application using telescope debugging and insight UI."
+                'desc' => 'Debug your application using telescope debugging and insight UI.',
             ],
             [
                 'route_name' => 'vapor-ui',
                 'title' => 'Vapor UI',
-                'desc' => "A beautiful dashboard accessible via your Vapor application that allows you to view/search your application's logs and failed queue jobs."
+                'desc' => "A beautiful dashboard accessible via your Vapor application that allows you to view/search your application's logs and failed queue jobs.",
             ],
             [
                 'route_name' => 'horizon.index',
                 'title' => 'Horizon',
-                'desc' => "Beautiful UI for monitoring your Redis driven Laravel queues."
-            ]
+                'desc' => 'Beautiful UI for monitoring your Redis driven Laravel queues.',
+            ],
         ];
 
         return array_filter($defaultApps, static function ($app) {
